1a16eb6f3004b8df29d487b3fead68aaaf95def2
Elke Hanschen <elke.hanschen@oetkerdigital.com>
2020-12-07 14:05:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Project smooth anchor jump/scroll
+
+### What is this about?
+
+- after clicking a link in nav, smooth anchor jump/scroll in that page to another section on that page
+- use simple JavaScript, no jQueryWhatsoever
+- the target sections are detected automatically by searching for the ids of the target (here, `href`)
+
+### This Repo is based on:
+
+- mentoring session happened in 2018
+
+### How to run this?
+
+- clone this repo
+- `cd` into project
+- open `index.html` in your browser of choice (in my case, built in/for Chrome)
---
+++ index.js
@@ -0,0 +1,35 @@
+const anchorJump = (function() {
+    let links
+
+    const setLinks = () => {
+        links = Array.from(document.querySelectorAll("[data-link"))
+    }
+
+    const linkAction = function(event) {
+        event.preventDefault()
+
+        const href = this.getAttribute('href')
+        const target = document.querySelector(href)
+
+        history.pushState(null, null, href)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+    }
+
+    const setLinkAction = () => {
+        links.forEach(link => link.addEventListener('click', linkAction))
+    }
+
+    const init = () => {
+        setLinks()
+        setLinkAction()
+    }
+
+    return {
+        init
+    }
+}) ()
+
+document.addEventListener('DOMContentLoaded', anchorJump.init)
